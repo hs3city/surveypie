8b673684c5951433e4f329238147dbe20eb5685e
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-07-17 19:26:15 +0200

    Allison Foster index calculation implementation (#20)
    * Allison Foster index calculation implementation
    
    * resolved comments
    
    * changed project name to src
    
    * Dependency fix
    
    * move comment to next line
    
    * added init with regard to python modules, if not needed to be deleted in future
    
    * addressed latest comment beside scipy as we need to have python version issue resolved first
    
    * remove unused import
    
    * added scipy to project dependencies
    
    ---------
    
    Co-authored-by: Filip Grabowski <grabfilip2007@gmail.com>

---
+++ unit_tests/test_01_allison_foster.py
@@ -0,0 +1,26 @@
+import random
+
+from src import allison_foster
+from src.allison_foster import AllisonFosterIndex
+
+
+def test_measures_calculation():
+    example_data = random.sample(range(1, 1000), 500)
+    result = allison_foster.compute_measures(example_data)
+    assert isinstance(result, AllisonFosterIndex)
+    assert isinstance(result.index, float)
+    assert isinstance(result.u_l, float)
+    assert isinstance(result.u_h, float)
+
+
+def test_on_example():
+    dataset = [1, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 5]
+
+    # estimated_median = 3
+    estimated_u_l = 7 / 6
+    estimated_u_h = 33 / 7
+    estimated_index = estimated_u_h - estimated_u_l
+    allison_foster_index = allison_foster.compute_measures(dataset)
+    assert estimated_u_l == allison_foster_index.u_l
+    assert estimated_u_h == allison_foster_index.u_h
+    assert estimated_index == allison_foster_index.index
