affb16dfcc0cf43908f7621b2ebd5784b250be7f
Marta Leszczynska <marta.monika.leszczynska@intel.com>
2023-09-05 16:13:20 +0200

    update column name in tests

--- unit_tests/test_02_general.py
+++ unit_tests/test_02_general.py
@@ -7,14 +7,15 @@ def test_on_example():
     dataset = [2, 1, 4, 3, 1, 5, 3, 3, 4, 2, 1,
                1, 3, 3, 4, 5, 5, 4, 3, 2, 2, 1, 2, 1]
     summary_stats = summary(dataset)
+    column_name = 'summary'
 
     assert isinstance(summary_stats, pd.DataFrame)
-    assert summary_stats.loc["count", 0] == len(dataset)
-    assert summary_stats.loc["min", 0] == 1
-    assert summary_stats.loc["max", 0] == 5
-    assert round(summary_stats.loc["mean", 0]) == sum(dataset) \
+    assert summary_stats.loc["count", column_name] == len(dataset)
+    assert summary_stats.loc["min", column_name] == 1
+    assert summary_stats.loc["max", column_name] == 5
+    assert round(summary_stats.loc["mean", column_name]) == sum(dataset) \
            / len(dataset)
-    assert round(summary_stats.loc["std", 0], 2) == 1.37
-    assert round(summary_stats.loc["25%", 0], 2) == 1.75
-    assert round(summary_stats.loc["50%", 0], 2) == 3.00
-    assert round(summary_stats.loc["75%", 0], 2) == 4.00
+    assert round(summary_stats.loc["std", column_name], 2) == 1.37
+    assert round(summary_stats.loc["25%", column_name], 2) == 1.75
+    assert round(summary_stats.loc["50%", column_name], 2) == 3.00
+    assert round(summary_stats.loc["75%", column_name], 2) == 4.00
