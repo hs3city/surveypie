26996829733c95432c1530f231623749e8bc78e7
Marta Leszczynska <marta.monika.leszczynska@intel.com>
2023-09-05 16:10:44 +0200

    line too long fixed

--- unit_tests/test_02_general.py
+++ unit_tests/test_02_general.py
@@ -4,14 +4,16 @@ from src.general import summary
 
 
 def test_on_example():
-    dataset = [2, 1, 4, 3, 1, 5, 3, 3, 4, 2, 1, 1, 3, 3, 4, 5, 5, 4, 3, 2, 2, 1, 2, 1]
+    dataset = [2, 1, 4, 3, 1, 5, 3, 3, 4, 2, 1,
+               1, 3, 3, 4, 5, 5, 4, 3, 2, 2, 1, 2, 1]
     summary_stats = summary(dataset)
 
     assert isinstance(summary_stats, pd.DataFrame)
     assert summary_stats.loc["count", 0] == len(dataset)
     assert summary_stats.loc["min", 0] == 1
     assert summary_stats.loc["max", 0] == 5
-    assert round(summary_stats.loc["mean", 0]) == sum(dataset) / len(dataset)
+    assert round(summary_stats.loc["mean", 0]) == sum(dataset) \
+           / len(dataset)
     assert round(summary_stats.loc["std", 0], 2) == 1.37
     assert round(summary_stats.loc["25%", 0], 2) == 1.75
     assert round(summary_stats.loc["50%", 0], 2) == 3.00
