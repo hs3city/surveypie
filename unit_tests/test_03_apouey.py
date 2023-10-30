43e38deff1f6dcddce37acbd689f9b9067963eb3
Marta Leszczynska <marta.monika.leszczynska@intel.com>
2023-10-30 14:37:51 +0100

    update tests

--- unit_tests/test_03_apouey.py
+++ unit_tests/test_03_apouey.py
@@ -1,21 +1,22 @@
 from src.apouey_index import get_apouey_index
 import unittest
 
-responses = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-    4, 4, 4, 4, 4,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5
-]
+DS = []
+DS.extend(10 * [1])
+DS.extend(10 * [2])
+DS.extend(10 * [3])
+DS.extend(5 * [4])
+DS.extend(15 * [5])
+
+INDICATORS = [1, 2, 3, 4, 5]
 
 
 def test_apouey_positive():
-    assert get_apouey_index(categories=5, responses=responses) == 0.55
+    assert round(get_apouey_index(INDICATORS, DS), 3) == 0.673
 
 
 class ApoueyTestCase(unittest.TestCase):
     def test_apouey_negative(self):
         with self.assertRaises(AttributeError):
-            get_apouey_index(categories=1, responses=responses)
-            get_apouey_index(categories=11, responses=responses)
+            get_apouey_index(categories=[1], responses=DS)
+            get_apouey_index(categories=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], responses=DS)
