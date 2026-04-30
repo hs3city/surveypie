00f6bf40a33adcca2f4fb3d0958246239a4fd661
Simon <31246246+SimonMolinsky@users.noreply.github.com>
2026-04-30 19:13:09 +0300

    Update entropy_index.py

--- src/surveypie/entropy_index.py
+++ src/surveypie/entropy_index.py
@@ -1,3 +1,7 @@
+from numpy.typing import ArrayLike
+import numpy as np
+from pydantic import field_validator
+
 from surveypie.structure.index_model import BaseIndex
 
 
@@ -14,11 +18,42 @@ class EntropyIndex(BaseIndex):
 
     a : float
         Parameter controlling sensitivity to income levels: large `a` is
-        especially sensitivie to existence of large incomes, and small
-        `a` is sensitive to small incomes.
-        # todo: a == 0 or a == 1 or a > 1 - description
+        especially sensitive to existence of large incomes, and small
+        `a` is sensitive to small incomes. The parameter equal to 0 is
+        the second Thiel measure or mean logarithmic deviation, and it assigns
+        larger weights to the bottom part of the distribution. The parameter
+        equal to 1 is the first Thiel measure - it assigns approximately equal
+        weights to all parts of the distribution. In cases when `a` is larger
+        than 1 it becomes more sensitive to existence of large outliers in
+        the distribution.
 
     """
 
     name: str = "Generalized entropy index"
-    a: float = 0
+    a: float = np.nan
+
+
+def entropy_index(data: ArrayLike, a: float):
+    """
+    Entropy Index calculation.
+
+    Parameters
+    ----------
+    data : ArrayLike
+        Vector with data.
+
+    a : float
+        Parameter controlling the entropy index.
+
+    Returns
+    -------
+
+    """
+    ...
+
+
+def thiel_first_measure():
+    ...
+
+def thiel_second_measure():
+    ...
